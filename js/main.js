a1aa8d2ea4ebdef9374459e4ebb2460c81d349ff
Mark Goericke <mark@javamark.de>
2019-12-03 19:16:21 +0100

    move static work examples into React components

--- js/main.js
+++ js/main.js
@@ -1,3 +1,34 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-console.log("Loaded react-dom")
+import ExampleWork from './example-work'
+
+const myWork = [
+  {
+    title: 'Cloud',
+    image: {
+      desc: 'example screenshot of a project involving code',
+      src: 'images/example1.png',
+      comment: 'Cloud'
+    }
+  },
+  {
+    title: 'Database',
+    image: {
+      desc: 'example screenshot of a project involving chemistry',
+      src: 'images/example2.png',
+      comment: ''
+    }
+  },
+  {
+    title: 'Misterio',
+    image: {
+      desc: 'example screenshot of a project involving cats',
+      src: 'images/example3.png',
+      comment: `“Bengal cat” by roberto shabs is licensed under CC BY 2.0
+           https://www.flickr.com/photos/37287295@N00/2540855181`
+    }
+  }
+];
+
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'))
